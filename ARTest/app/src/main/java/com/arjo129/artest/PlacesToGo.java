c639176ba0bb7b40a30b1b8e5b53e634c2d1b4e2
chelseyong <chelseyong@gmail.com>
2018-05-28 14:26:03 +0800

    wifi-scanning updated

--- ARTest/app/src/main/java/com/arjo129/artest/PlacesToGo.java
+++ ARTest/app/src/main/java/com/arjo129/artest/PlacesToGo.java
@@ -1,15 +1,26 @@
 package com.arjo129.artest;
 
+import android.Manifest;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.GridView;
+import android.widget.Toast;
 
-public class PlacesToGo extends AppCompatActivity {
+import java.util.List;
 
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.AppSettingsDialog;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class PlacesToGo extends AppCompatActivity
+        implements EasyPermissions.PermissionCallbacks{
+
+    private static final int LOCATION_PERMISSION = 22;
     private GridView gridView;
     String[] places = {
             "Favourites",
@@ -51,9 +62,51 @@ public class PlacesToGo extends AppCompatActivity {
             startActivity(compassIntent);
         }
         if(item.getItemId() == R.id.activity_wifi){
-            Intent wifiIntent = new Intent(this, WifiActivity.class);
-            startActivity(wifiIntent);
+            wifiScanning();
         }
         return super.onOptionsItemSelected(item);
+    }
+    @AfterPermissionGranted(LOCATION_PERMISSION)
+    private void wifiScanning(){
+        String[] perms = {Manifest.permission.ACCESS_FINE_LOCATION};
+        if(EasyPermissions.hasPermissions(this, perms)){
+            openWifiIntent();
+
+        }
+        else EasyPermissions.requestPermissions(this, "We need permissions to do Wifi Scanning",
+                LOCATION_PERMISSION, perms);
+    }
+
+    public void openWifiIntent(){
+        Intent wifiIntent = new Intent(this, WifiActivity.class);
+        startActivity(wifiIntent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        EasyPermissions.onRequestPermissionsResult(requestCode,permissions,grantResults,this);
+
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+        if(EasyPermissions.somePermissionPermanentlyDenied(this, perms)){
+            new AppSettingsDialog.Builder(this).build().show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == AppSettingsDialog.DEFAULT_SETTINGS_REQ_CODE){
+            wifiScanning();
+        }
     }
 }
--- ARTest/app/src/main/java/com/arjo129/artest/WifiActivity.java
+++ ARTest/app/src/main/java/com/arjo129/artest/WifiActivity.java
@@ -2,10 +2,17 @@ package com.arjo129.artest;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
+import android.provider.Settings;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,12 +20,9 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class WifiActivity extends AppCompatActivity {
@@ -27,6 +31,8 @@ public class WifiActivity extends AppCompatActivity {
     private Button buttonScan;
     private List<ScanResult>results;
     private static int size = 0;
+    private boolean gps_enabled = false;
+    private boolean network_enabled = false;
 
     ArrayList<String>arrayList = new ArrayList<>();
     ArrayAdapter adapter;
@@ -40,6 +46,8 @@ public class WifiActivity extends AppCompatActivity {
         buttonScan.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
+                adapter.clear();
+                adapter.notifyDataSetChanged();
                 scanWifiNetworks();
             }
         });
@@ -53,27 +61,30 @@ public class WifiActivity extends AppCompatActivity {
         }
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
         listView.setAdapter(adapter);
+
         scanWifiNetworks();
     }
 
-//    @Override
-//    protected void onPause() {
-//        super.onPause();
-//        unregisterReceiver(wifi_receiver);
-//    }
-//
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        unregisterReceiver(wifi_receiver);
-//    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gps_enabled = false;
+        network_enabled = false;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        scanWifiNetworks();
+    }
 
     public void scanWifiNetworks(){
-        arrayList.clear();
-        registerReceiver(wifi_receiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
-
-        wifi.startScan();
-        Toast.makeText(this, "Scanning....", Toast.LENGTH_SHORT).show();
+        enableLocation();
+        if(gps_enabled && network_enabled){
+            registerReceiver(wifi_receiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+            wifi.startScan();
+            Toast.makeText(this, "Scanning....", Toast.LENGTH_SHORT).show();
+        }
     }
 
     BroadcastReceiver wifi_receiver = new BroadcastReceiver() {
@@ -90,4 +101,41 @@ public class WifiActivity extends AppCompatActivity {
             }
         }
     };
+
+
+
+    public void enableLocation(){
+        LocationManager lm = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+        try{
+            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        }catch (Exception e){
+            Log.d("WifiGPS", "GPS cannot be enabled");
+        }
+        try{
+            network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        }catch (Exception e){
+            Log.d("WifiGPS", "Network cannot be enabled");
+        }
+
+        if(!gps_enabled && !network_enabled){
+            AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+            dialog.setPositiveButton(getResources().getString(R.string.open_location_settings), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    Intent myIntent = new Intent( Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(myIntent);
+                    //get gps
+                }
+            });
+            dialog.setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    onBackPressed();
+                }
+            });
+            dialog.show();
+
+        }
+    }
+
 }
